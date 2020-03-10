d17a93bd5a42086abc45cc3631ba793ff5092444
Erik Kopcha <erickopcha@gmail.com>
2020-03-10 20:56:01 +0200

    budget

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,29 @@
 'use strict';
 
-// Типы данных
-let number = 5;
-let string = 'hello!';
-let sym = Symbol();
-let boolean = true;
-null;
-undefined;
-let obj = {};
+// // Типы данных
+// let number = 5;
+// let string = 'hello!';
+// let sym = Symbol();
+// let boolean = true;
+// null;
+// undefined;
+// let obj = {};
 
-// Вывод infinity && NaN
-console.log(4 / 0);
-console.log('string' * 9);
+// // Вывод infinity && NaN
+// console.log(4 / 0);
+// console.log('string' * 9);
 
-///Вывод undefined
-let something;
-console.log(something);
+// ///Вывод undefined
+// let something;
+// console.log(something);
 
-// Объект
-let persone = {
-  name: 'Alex',
-  age: 25,
-  isMarried: false
-};
-console.log(persone.name);
+// // Объект
+// let persone = {
+//   name: 'Alex',
+//   age: 25,
+//   isMarried: false
+// };
+// console.log(persone.name);
 
 // Массив
 // let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
@@ -55,3 +55,54 @@ console.log(persone.name);
 //     isClose = true;
 
 // console.log(isChecked && !isClose);
+
+// Условия 
+let num = 50;
+
+if (num < 49) {
+  console.log('No!');
+} else if (num > 100) {
+  console.log('No!');
+} else {
+  console.log('Yes!');
+}
+
+(num == 50) ? console.log('Yes!') : console.log('No!');
+
+switch (num) {
+  case num < 49:
+    console.log('No!');
+    break;
+  case num > 100:
+    console.log('No!');
+    break;
+  case num > 80:
+    console.log('No!');
+    break;
+  case 50:
+    console.log('Yes!');
+    break;
+  default:
+    console.log('error');
+    break;
+}
+
+// Циклы
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+  if (i == 6) {
+    // break;
+    continue;
+  }
+  console.log(i);
+}
