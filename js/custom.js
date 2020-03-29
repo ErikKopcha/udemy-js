4abe866fd0208fd51e36f17ac74338bb1ad856f3
Erik Kopcha <erickopcha@gmail.com>
2020-03-29 18:46:58 +0300

    feat: hw 4

--- js/custom.js
+++ js/custom.js
@@ -155,3 +155,133 @@
 // // console.log(Math.round(twelve));
 // console.log(parseInt(twelve));
 // console.log(parseFloat(twelve));
+
+
+
+// callback
+// function first() {
+//   setTimeout( function() {
+//     console.log(1);
+//   }, 500);
+// }
+
+// function second() {
+//   console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//   console.log('i learn ' + lang);
+//   callback();
+// }
+
+// learnJS('JavaScript',  function() {
+//   console.log('3 lesson');
+// });
+
+// function done() {
+//   console.log('3 lesson');
+// }
+// learnJS('JavaScript', done);
+
+
+
+// object
+// let options = {
+//   width: 1024,
+//   height: 1024,
+//   name: 'test',
+// };
+
+// console.log(options.name);
+ 
+// options.bool = false;
+// options.colors = {
+//   border: 'black',
+//   background: 'red'
+// };
+
+// delete options.bool;
+
+// console.log(options);
+
+// for (let key in options) {
+//   console.log('Свойство ' + key + ' имеет значение ' + options[key]);
+// }
+
+// console.log(Object.keys(options).length);
+
+
+
+
+// massive
+// let arr = ['first', 2, 3, 'four', 5];
+
+// item - каждый элеиент
+// і - номер каждого элемента
+// mass - массив какой используется
+// arr.forEach(function (item, i, mass) {
+//   console.log(i + ': ' + item + ' (массив: ' + mass + ')');
+// });
+
+// arr[99] = 99; // нельзя так
+// console.log(arr.length); // выводит не количество элем, а последний индекс + 1
+// console.log(arr);
+
+// // arr.pop(); // удалить последний элемент
+// // arr.push('5'); // добавить элемент
+// // arr.shift(); // удаляет первый элемент
+// // arr.unshift('1'); // добавляет первый элемент
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// console.log(arr);
+
+// let mass = [1, 3, 5, 6, 7];
+
+// // чтобы получить только значение массива - of
+// for (let key of mass) {
+//   console.log(key);
+// }
+
+// let ans = prompt('', ''),
+//     arr = [];
+
+// arr = ans.split(','); // разбивает элементы массива
+// console.log(arr);
+
+// let arr = ['ass', 'qwewew', 'qweww'],
+//     i = arr.join(', '); // склеивает элементы массива в строки
+// console.log(i);
+
+// Сортировка по алфавиту (строки)
+// let arr = ['ass', 'qwewew', 'qweww'],
+//     i = arr.sort();
+// console.log(arr);
+
+// Сортировка по алфавиту (числа)
+// let arr = ['12', '1', '111'],
+//     i = arr.sort(compareNum);
+
+// function compareNum(a, b) {
+//   return a-b;
+// }
+
+// console.log(arr);
+
+// let soldier = {
+//   health: 400,
+//   armor: 100
+// };
+
+// let john = {
+//   health: 100
+// };
+
+// john.__proto__ = soldier; // добавляет Джона к потомкам soldier
+// console.log(john);
+// console.log(john.armor);
--- js/script.js
+++ js/script.js
@@ -1,23 +1,71 @@
-// 'use strict';
+'use strict';
 
-// let money = prompt("Ваш бюджет на месяц?", ''),
-// 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
 
-// let appData = {
-// 	budget: money,
-// 	expenses: {},
-// 	optionalExpenses: {},
-// 	income: [],
-// 	timeData: time,
-// 	savings: false
-// };
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", '');
+  time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-// let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 	a2 = prompt("Во сколько обойдется?", ''),
-// 	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 	a4 = prompt("Во сколько обойдется?", '');
+  while (isNaN(money) || money == '' || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", '');
+  }
+}
 
-// appData.expenses.a1 = a2;
-// appData.expenses.a3 = a4;
+start();
 
-// alert(appData.budget / 30);
+let appData = {
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  timeData: time,
+  savings: true,
+  chooseExpenses: function () {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+
+      if ((typeof (a)) === 'string' && (typeof (a) != null) && (typeof (b) != null) && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+      } else {
+        i--;
+      }
+    }
+  },
+  detectDayBudget: function () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+  },
+  detectLevel: function () {
+    if (appData.moneyPerDay < 100) {
+      console.log('Минимальный уровень достатка');
+    } else if (appData.moneyPerDay > 100) {
+      console.log('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 2000) {
+      console.log('Высокий уровень достатка');
+    } else {
+      console.log('Произошла ошибка');
+    }
+  },
+  checkSavings: function () {
+    if (appData.savings == true) {
+      let save = +prompt('Какова сумма накоплений?');
+      let percent = +prompt('Под какой процент?');
+
+      appData.monthIncome = save / 100 / 12 * percent;
+      alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+    }
+  },
+  chooseOptExpenses: function () {
+    for (let i = 1; i < 3; i++) {
+      let opt = prompt('Статья неоязательных расходов?', '');
+      appData.optionalExpenses[i] = opt;
+    }
+  },
+  chooseIncome: function () {
+    let items = prompt('Что принесет допольнительный доход? (Перечислите через запятую)', '');
+    appData.income = items.split(', ');
+    appData.income.push(prompt('Может что-то еще?'));
+    appData.income.sort();
+  }
+};
