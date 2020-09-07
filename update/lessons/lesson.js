da2c89727db4a4a43097fa547bd88d7812104374
Erik <erickopcha@gmail.com>
2020-09-08 01:01:24 +0300

    get set

--- update/lessons/lesson.js
+++ update/lessons/lesson.js
@@ -263,18 +263,84 @@
 
 
 ///////////////////////////////////////////////////////// get set
-const persone = {
-  name: 'Alex',
-  age: 25,
+// const persone = {
+//   name: 'Alex',
+//   age: 25,
 
-  get userAge() {
-    return this.age;
-  },
+//   get userAge() {
+//     return this.age;
+//   },
 
-  set userAge(num) {
-    this.age = num;
-  }
-};
+//   set userAge(num) {
+//     this.age = num;
+//   }
+// };
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+// console.log(persone.userAge = 30);
+// console.log(persone.userAge);
+
+//////////////////////////////////////////////////////// інкапсуляція
+// function User(name, age) {
+//   this.name = name;
+//   let userAge = age;
+
+//   this.say = function() {
+//     console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+//   };
+
+//   this.getAge = function() {
+//     return userAge;
+//   };
+
+//   this.setAge = function(age) {
+//     if (typeof age === 'number' && age > 0 && age < 110) {
+//       userAge = age;
+//     } else {
+//       console.log('Недопустимое значение!');
+//     }
+//   };
+// }
+
+// const ivan = new User('Ivan', 27);
+// console.log(ivan.getAge());
+
+// ivan.setAge(30);
+// ivan.setAge(300);
+
+// console.log(ivan.getAge());
+
+// ivan.userAge = 30;
+
+// ivan.say();
+
+
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this._age = age;
+//   }
+
+//   say() {
+//     console.log(`Имя пользователя: ${this.name}, возраст ${this._age}`);
+//   }
+
+//   get age() {
+//     return this._age;
+//   }
+
+//   set age(age) {
+//     if (typeof age === 'number' && age > 0 && age < 110) {
+//       this._age = age;
+//     } else {
+//       console.log('Недопустимое значение!');
+//     }
+//   }
+// }
+
+// const ivan = new User('Ivan', 27);
+
+// ivan.age = 99;
+
+// console.log(ivan.age);
+
+// ivan.say();
